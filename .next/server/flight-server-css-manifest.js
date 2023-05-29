59bee4ad4b5ab6a86a4df21ff9be3be0bd43dc87
camila <cardenascastrolauracamila7@gmail.com>
2023-05-29 13:18:06 -0500

    se instalo react en el proyecto

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/opt/formulario/src/app/layout.js": [
+      "/opt/formulario/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/opt/formulario/src/app/globals.css"
+    ],
+    "/opt/formulario/src/app/page.js": [
+      "/opt/formulario/src/app/page.module.css"
+    ]
+  },
+  "cssModules": {
+    "/opt/formulario/src/app/page": [
+      "/opt/formulario/src/app/page.module.css",
+      "/opt/formulario/src/app/globals.css",
+      "/opt/formulario/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
---
+++ .next/static/webpack/app/page.b901067e400e68e6.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fopt%2Fformulario%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2Fopt%2Fformulario%2Fsrc%2Fapp%2Fpage.module.css&server=false!":
+/*!**********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fopt%2Fformulario%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2Fopt%2Fformulario%2Fsrc%2Fapp%2Fpage.module.css&server=false! ***!
+  \**********************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/image.js */ \"(app-client)/./node_modules/next/dist/client/image.js\", 23));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./src/app/page.module.css */ \"(app-client)/./src/app/page.module.css\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPSUyRm9wdCUyRmZvcm11bGFyaW8lMkZub2RlX21vZHVsZXMlMkZuZXh0JTJGZGlzdCUyRmNsaWVudCUyRmltYWdlLmpzJm1vZHVsZXM9JTJGb3B0JTJGZm9ybXVsYXJpbyUyRnNyYyUyRmFwcCUyRnBhZ2UubW9kdWxlLmNzcyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLHlNQUEyRjtBQUMzRiIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvP2ZjYjEiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCIvb3B0L2Zvcm11bGFyaW8vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9jbGllbnQvaW1hZ2UuanNcIik7XG5pbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIi9vcHQvZm9ybXVsYXJpby9zcmMvYXBwL3BhZ2UubW9kdWxlLmNzc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2Fopt%2Fformulario%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Fimage.js&modules=%2Fopt%2Fformulario%2Fsrc%2Fapp%2Fpage.module.css&server=false!\n"));
+
+/***/ })
+
+});
